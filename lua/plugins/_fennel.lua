6960eed64cf4f7b7d36a882519b237dd076c0837
IwnuplyNotTyan <wolflast21@gmail.com>
2025-07-05 08:20:37 +0300

    disable fennel support, btw unused

---
+++ lua/plugins/_fennel.lua
@@ -0,0 +1,18 @@
+--return {
+--{
+--  "rktjmp/hotpot.nvim",
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    require("hotpot").setup({
+--      provide_require_fennel = true,
+--      enable_hotpot_diagnostics = true,
+--      compiler = {
+--        modules = {
+--          correlate = true
+--        }
+--      }
+--    })
+--  end
+--}
+--}
