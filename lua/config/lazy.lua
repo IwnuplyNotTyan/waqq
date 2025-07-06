08d357c436db63a05f17337af65fad69d1652090
IwnuplyNotTyan <wolflast21@gmail.com>
2025-07-06 12:35:20 +0300

    idk, mirror

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,14 +1,14 @@
 require("lazy").setup({
   spec = {
-    { import = "plugins"},	 -- All plugins
-    { import = "plugins.lsp"},   -- LSP
-    { import = "plugins.hydra"}, -- Hydra
-    { import = "plugins.ui"},    -- UI
-    { import = "plugins.ai"},    -- AI
-    { import = "plugins.render"},-- Render Markdown, Norg
-    { import = "plugins.git"},   -- Git
-    { import = "plugins.tool"},  -- Tools
-    { import = "plugins.etc"},   -- Etc
+    { import = "plugins"},	  -- All plugins
+    { import = "plugins.lsp"},    -- LSP
+    { import = "plugins.hydra"},  -- Hydra
+    { import = "plugins.ui"},     -- UI
+    { import = "plugins.ai"},     -- AI
+    { import = "plugins.render"}, -- Render Markdown, Norg
+    { import = "plugins.git"},    -- Git
+    { import = "plugins.tool"},   -- Tools
+    { import = "plugins.etc"},    -- Etc
   },
   defaults = {
     lazy = false,
