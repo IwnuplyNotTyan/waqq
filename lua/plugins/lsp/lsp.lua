5d293b4d00e3323917fb7f1dd80a83623601d6fd
IwnuplyNotTyan <wolflast21@gmail.com>
2025-08-12 10:08:55 +0300

    return and fixed old lualine

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -237,12 +237,5 @@ local handlers = {
   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
 }
-
--- Symbol icons for lsp-kind
-local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
   	end},
 }
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,230 +1,140 @@
-return {'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		-- Кастомная цветовая схема на основе вашей темы
-		local colors = {
-			bg = '#0B0F10',
-			fg = '#c5c8c9',
-			black = '#101415',
-			black_light = '#131718',
-			red = '#df5b61',
-			red_light = '#ee6a70',
-			green = '#87c7a1',
-			green_light = '#96d6b0',
-			yellow = '#de8f78',
-			yellow_light = '#ffb29b',
-			blue = '#6791c9',
-			blue_light = '#7ba5dd',
-			magenta = '#bc83e3',
-			magenta_light = '#cb92f2',
-			cyan = '#70b9cc',
-			cyan_light = '#7fc8db',
-			white = '#c4c4c4',
-			white_light = '#cccccc',
-		}
+return {
+	{'nvim-lualine/lualine.nvim', config = function()
+require('lualine').setup()
+	local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
 
-		local custom_theme = {
-			normal = {
-				a = { fg = colors.bg, bg = colors.blue, gui = 'bold' },
-				b = { fg = colors.fg, bg = colors.black_light },
-				c = { fg = colors.fg, bg = colors.bg },
-			},
-			insert = {
-				a = { fg = colors.bg, bg = colors.green, gui = 'bold' },
-				b = { fg = colors.fg, bg = colors.black_light },
-				c = { fg = colors.fg, bg = colors.bg },
-			},
-			visual = {
-				a = { fg = colors.bg, bg = colors.magenta, gui = 'bold' },
-				b = { fg = colors.fg, bg = colors.black_light },
-				c = { fg = colors.fg, bg = colors.bg },
-			},
-			command = {
-				a = { fg = colors.bg, bg = colors.yellow, gui = 'bold' },
-				b = { fg = colors.fg, bg = colors.black_light },
-				c = { fg = colors.fg, bg = colors.bg },
-			},
-			replace = {
-				a = { fg = colors.bg, bg = colors.red, gui = 'bold' },
-				b = { fg = colors.fg, bg = colors.black_light },
-				c = { fg = colors.fg, bg = colors.bg },
-			},
-			inactive = {
-				a = { fg = colors.white, bg = colors.black },
-				b = { fg = colors.white, bg = colors.black },
-				c = { fg = colors.white, bg = colors.black },
-			},
-		}
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+local spaces = {
+	function()
+		return " "
+	end,
+	padding = -1,
+}
 
-		require('lualine').setup {
-			options = {
-				icons_enabled = true,
-				theme = custom_theme,
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				always_show_tabline = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-					refresh_time = 16,
-					events = {
-						'WinEnter',
-						'BufEnter',
-						'BufWritePost',
-						'SessionLoadPost',
-						'FileChangedShellPost',
-						'VimResized',
-						'Filetype',
-						'CursorMoved',
-						'CursorMovedI',
-						'ModeChanged',
-					},
-				}
+local diagnostics = {
+"diagnostics",
+sources = { "nvim_diagnostic" },
+sections = {
+	"error",
+	"warn",
+	"hint",
+},
+symbols = {
+	error = " ",
+	warn = " ",
+	hint = " ",
+	info = " ",
+},
+colored = true,
+always_visible = false,
+}
+
+local branch = {
+"branch",
+icon = " 󰊢 ",
+separator = { left = "", right = "" },
+padding = 0.1
+}
+
+local diff = {
+"diff",
+colored = true,
+symbols = {
+	added = " ",
+	modified = " ",
+	removed = " ",
+},
+separator = { left = "", right = "" },
+}
+
+local filetype = {
+"filetype",
+icons_enabled = true,
+}
+
+local location = {
+"location",
+}
+
+local custom_icons = {
+function()
+	return " "
+end,
+separator = { left = "", right = "" },
+}
+
+local modes = {
+"mode",
+separator = { left = "", right = "" },
+padding = 0.8,
+}
+
+lualine.setup({
+options = {
+	globalstatus = true,
+	icons_enabled = true,
+	theme = "auto",
+	component_separators = { left = "", right = "" },
+	section_separators = { left = "", right = "" },
+	disabled_filetypes = { "alpha", "dashboard", "packer", "neo-tree", "nvim-tree" },
+	always_divide_middle = true,
+},
+sections = {
+	lualine_a = {
+		custom_icons,
+		modes,
+	},
+	lualine_b = {
+	},
+	lualine_c = {
+    spaces,
+		{
+			"filetype",
+			icon_only = true,
+			colored = true,
+			padding = 1,
+			color = { bg = "#2a2c3f" },
+			separator = { left = "", right = "" },
+		},
+		{
+			"filename",
+      file_status = false,
+			padding = 0.3,
+			separator = { left = "", right = "" },
+			color = { bg = "#2a2c3f" },
+		},
+		branch,
+		diff,
+	},
+			lualine_x = {
+			diagnostics,
+			{
+				function()
+					return " "
+				end,
+				separator = { left = "", right = "" },
+				color = { bg = "#ffb29b", fg = "#101415" },
+				padding = 0.3,
 			},
-			sections = {
-				lualine_a = {
-					{
-						'mode',
-						icon = '',
-						fmt = function(str)
-							return str:sub(1,1):upper()..str:sub(2):lower()
-						end,
-					}
-				},
-				lualine_b = {
-					{
-						'branch',
-						icon = '',
-						color = { fg = colors.cyan_light, gui = 'bold' }
-					},
-					{
-						'diff',
-						symbols = {
-							added = ' ',
-							modified = ' ',
-							removed = ' '
-						},
-						diff_color = {
-							added = { fg = colors.green_light },
-							modified = { fg = colors.yellow_light },
-							removed = { fg = colors.red_light },
-						},
-					},
-					{
-						'diagnostics',
-						symbols = {
-							error = ' ',
-							warn = ' ',
-							info = ' ',
-							hint = ' ',
-						},
-						diagnostics_color = {
-							error = { fg = colors.red_light },
-							warn = { fg = colors.yellow_light },
-							info = { fg = colors.blue_light },
-							hint = { fg = colors.cyan_light },
-						},
-					}
-				},
-				lualine_c = {
-					{
-						'filename',
-						file_status = true,
-						newfile_status = false,
-						path = 1,
-						shorting_target = 40,
-						symbols = {
-							modified = ' ●',
-							readonly = ' ',
-							unnamed = '[Без названия]',
-							newfile = '[Новый]',
-						},
-						color = { fg = colors.white_light, gui = 'italic' }
-					}
-				},
-				lualine_x = {
-					-- LSP статус
-					{
-						function()
-							local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-							if next(clients) == nil then
-								return ''
-							end
-							local client_names = {}
-							for _, client in pairs(clients) do
-								table.insert(client_names, client.name)
-							end
-							return ' ' .. table.concat(client_names, ', ')
-						end,
-						color = { fg = colors.magenta_light },
-					},
-					{
-						'encoding',
-						icon = '',
-						color = { fg = colors.blue_light }
-					},
-					{
-						'fileformat',
-						symbols = {
-							unix = ' Unix',
-							dos = ' DOS',
-							mac = ' Mac',
-						},
-						color = { fg = colors.blue_light }
-					},
-					{
-						'filetype',
-						colored = true,
-						icon_only = false,
-						icon = { align = 'right' },
-						color = { fg = colors.cyan_light }
-					}
-				},
-				lualine_y = {
-					{
-						'progress',
-						icon = '',
-						color = { fg = colors.green_light, gui = 'bold' }
-					}
-				},
-				lualine_z = {
-					{
-						'location',
-						icon = '',
-						color = { fg = colors.yellow_light, gui = 'bold' }
-					}
-				}
+			"progress",
+			{
+				function()
+					return "󰆤 "
+				end,
+				separator = { left = "", right = "" },
+				color = { bg = "#cb92f2", fg = "#101415" },
+				padding = 0.1,
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {
-					{
-						'filename',
-						color = { fg = colors.white }
-					}
-				},
-				lualine_x = {
-					{
-						'location',
-						color = { fg = colors.white }
-					}
-				},
-				lualine_y = {},
-				lualine_z = {}
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = { 'nvim-tree', 'lazy', 'mason', 'trouble' }
-		}
-	end
+			location,
+		},
+	lualine_y = {},
+	lualine_z = {},
+},
+})
+end},
 }
